b544d89c163aebdc170d4f9cfa70ba10ad8debcb
Mauro Nunes <mauraoemau@hotmail.com>
2021-05-06 23:51:45 -0300

    View post

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/visualizar', function (req, res, next) {
-  res.send("page view");
+router.get('/visualizar/:id', function (req, res, next) {
+  res.render('posts/view');
 });
 
 module.exports = router;
