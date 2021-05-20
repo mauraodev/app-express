cf301acfc46d092b7a6aaf2f382cbe50b2eb2b57
Mauro Nunes <maurao.dev@gmail.com>
2021-05-20 15:30:37 -0300

    Start

--- routes/users.ts
+++ routes/users.ts
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
+const router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const users = ['Mauro', 'Poli', 'Elena']
 
-  res.json(['Mauro', 'Poli', 'Elena']);
+  res.json(['Mauro', 'Poli', 'Elena', 'Melissa']);
 });
 
 module.exports = router;
