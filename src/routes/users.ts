df633d6cf43926ae00d60d94ed5167a284d88361
Mauro Nunes <maurao.dev@gmail.com>
2021-05-24 09:57:02 -0300

    User add route post

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,16 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', (req: Request, res: Response) => {
   const users = ['Mauro', 'Poli', 'Elena']
-
   res.json(users)
+})
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Sucesso ao adicionar usuário',
+  })
 })
 
 export default router
